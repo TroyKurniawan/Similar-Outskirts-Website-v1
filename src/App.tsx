ab195248147712161649c86495abad8771fa02b8
TroyKurniawan <TroyKurniawan97@gmail.com>
2024-07-06 11:21:02 -0700

    Init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen bg-slate-800 grid justify-center content-center">
+      <h1 className="text-white text-9xl font-bold m-2">Hello World!</h1>
+      <h2 className="text-white text-7xl m-2">My name is Similar Outskirts!</h2>
     </div>
   );
 }

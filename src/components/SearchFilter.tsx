260964611f78df42689d9c0d95986b9328f3a1d2
TroyKurniawan <TroyKurniawan97@gmail.com>
2024-07-11 00:45:05 -0700

    Added calendar icon, changed icon colors to match text

--- src/components/SearchFilter.tsx
+++ src/components/SearchFilter.tsx
@@ -32,7 +32,7 @@ function SearchFilter() {
             height="24px"
             viewBox="0 -960 960 960"
             width="24px"
-            fill="gray"
+            fill="#6b7280"
           >
             <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
           </svg>
@@ -58,7 +58,7 @@ function SearchFilter() {
               height="24px"
               viewBox="0 -960 960 960"
               width="24px"
-              fill="gray"
+              fill="#6b7280"
             >
               <path d="M418-340q24 24 62 23.5t56-27.5l224-336-336 224q-27 18-28.5 55t22.5 61Zm62-460q59 0 113.5 16.5T696-734l-76 48q-33-17-68.5-25.5T480-720q-133 0-226.5 93.5T160-400q0 42 11.5 83t32.5 77h552q23-38 33.5-79t10.5-85q0-36-8.5-70T766-540l48-76q30 47 47.5 100T880-406q1 57-13 109t-41 99q-11 18-30 28t-40 10H204q-21 0-40-10t-30-28q-26-45-40-95.5T80-400q0-83 31.5-155.5t86-127Q252-737 325-768.5T480-800Zm7 313Z" />
             </svg>
@@ -86,12 +86,25 @@ function SearchFilter() {
       <h2 className="font-bold my-2">Release Year</h2>
 
       <div className="flex place-content-between place-items-center">
-        <input
-          className="bg-slate-800 w-36 h-8 text-center"
-          placeholder="Lower Year"
-          type="number"
-          defaultValue={2014}
-        />
+        <div className="flex">
+          <div className="absolute m-1 pl-1 scale-90">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="22px"
+              viewBox="0 -960 960 960"
+              width="22px"
+              fill="#6b7280"
+            >
+              <path d="M200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Zm280 240q-17 0-28.5-11.5T440-440q0-17 11.5-28.5T480-480q17 0 28.5 11.5T520-440q0 17-11.5 28.5T480-400Zm-160 0q-17 0-28.5-11.5T280-440q0-17 11.5-28.5T320-480q17 0 28.5 11.5T360-440q0 17-11.5 28.5T320-400Zm320 0q-17 0-28.5-11.5T600-440q0-17 11.5-28.5T640-480q17 0 28.5 11.5T680-440q0 17-11.5 28.5T640-400ZM480-240q-17 0-28.5-11.5T440-280q0-17 11.5-28.5T480-320q17 0 28.5 11.5T520-280q0 17-11.5 28.5T480-240Zm-160 0q-17 0-28.5-11.5T280-280q0-17 11.5-28.5T320-320q17 0 28.5 11.5T360-280q0 17-11.5 28.5T320-240Zm320 0q-17 0-28.5-11.5T600-280q0-17 11.5-28.5T640-320q17 0 28.5 11.5T680-280q0 17-11.5 28.5T640-240Z" />
+            </svg>
+          </div>
+          <input
+            className="bg-slate-800 w-36 h-8 pl-4 text-center"
+            placeholder="Lower Year"
+            type="number"
+            defaultValue={2014}
+          />
+        </div>
         <p>to</p>
         <input
           className="bg-slate-800 w-36 h-8 text-center"
@@ -114,7 +127,7 @@ function SearchFilter() {
             height="24px"
             viewBox="0 -960 960 960"
             width="24px"
-            fill="gray"
+            fill="#6b7280"
           >
             <path d="M400-120q-66 0-113-47t-47-113q0-66 47-113t113-47q23 0 42.5 5.5T480-418v-422h240v160H560v400q0 66-47 113t-113 47Z" />
           </svg>

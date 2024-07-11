40f0b4b36b4b4ffe83771add70ce78ac2344ce2e
TroyKurniawan <TroyKurniawan97@gmail.com>
2024-07-11 14:21:12 -0700

    MusicEntry takes into account EPs

--- src/components/music/MusicEntry.tsx
+++ src/components/music/MusicEntry.tsx
@@ -95,51 +95,54 @@ function MusicEntry({ entry }: MusicEntryProps) {
         </div>
 
         {/* Side Box */}
-        <div
-          className="grid justify-start place-content-between w-1/4
+        {entry.type == "single" && (
+          <div
+            className="grid justify-start place-content-between w-1/4
                       text-gray-500 text-sm space-y-2"
-        >
-          {/* Length */}
-          <div className="flex w-16 space-x-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="17px"
-              viewBox="0 -960 960 960"
-              width="17px"
-              fill="#6b7280"
-            >
-              <path d="M360-840v-80h240v80H360Zm80 440h80v-240h-80v240Zm40 320q-74 0-139.5-28.5T226-186q-49-49-77.5-114.5T120-440q0-74 28.5-139.5T226-694q49-49 114.5-77.5T480-800q62 0 119 20t107 58l56-56 56 56-56 56q38 50 58 107t20 119q0 74-28.5 139.5T734-186q-49 49-114.5 77.5T480-80Zm0-80q116 0 198-82t82-198q0-116-82-198t-198-82q-116 0-198 82t-82 198q0 116 82 198t198 82Zm0-280Z" />
-            </svg>
-            <p className="ml-1">{length_string}</p>
-          </div>
-          {/* Tempo */}
-          <div className="flex w-32 space-x-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="17px"
-              viewBox="0 -960 960 960"
-              width="17px"
-              fill="#6b7280"
-            >
-              <path d="M418-340q24 24 62 23.5t56-27.5l224-336-336 224q-27 18-28.5 55t22.5 61Zm62-460q59 0 113.5 16.5T696-734l-76 48q-33-17-68.5-25.5T480-720q-133 0-226.5 93.5T160-400q0 42 11.5 83t32.5 77h552q23-38 33.5-79t10.5-85q0-36-8.5-70T766-540l48-76q30 47 47.5 100T880-406q1 57-13 109t-41 99q-11 18-30 28t-40 10H204q-21 0-40-10t-30-28q-26-45-40-95.5T80-400q0-83 31.5-155.5t86-127Q252-737 325-768.5T480-800Zm7 313Z" />
-            </svg>
-            <p className="ml-1">{tempo_string + " bpm"}</p>
-          </div>
+          >
+            {/* Length */}
+            <div className="flex w-16 space-x-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="17px"
+                viewBox="0 -960 960 960"
+                width="17px"
+                fill="#6b7280"
+              >
+                <path d="M360-840v-80h240v80H360Zm80 440h80v-240h-80v240Zm40 320q-74 0-139.5-28.5T226-186q-49-49-77.5-114.5T120-440q0-74 28.5-139.5T226-694q49-49 114.5-77.5T480-800q62 0 119 20t107 58l56-56 56 56-56 56q38 50 58 107t20 119q0 74-28.5 139.5T734-186q-49 49-114.5 77.5T480-80Zm0-80q116 0 198-82t82-198q0-116-82-198t-198-82q-116 0-198 82t-82 198q0 116 82 198t198 82Zm0-280Z" />
+              </svg>
+              <p className="ml-1">{length_string}</p>
+            </div>
 
-          {/* Key */}
-          <div className="flex w-32 space-x-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="17px"
-              viewBox="0 -960 960 960"
-              width="17px"
-              fill="#6b7280"
-            >
-              <path d="M400-120q-66 0-113-47t-47-113q0-66 47-113t113-47q23 0 42.5 5.5T480-418v-422h240v160H560v400q0 66-47 113t-113 47Z" />
-            </svg>
-            <p className="ml-1">{keysig_string}</p>
+            {/* Tempo */}
+            <div className="flex w-32 space-x-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="17px"
+                viewBox="0 -960 960 960"
+                width="17px"
+                fill="#6b7280"
+              >
+                <path d="M418-340q24 24 62 23.5t56-27.5l224-336-336 224q-27 18-28.5 55t22.5 61Zm62-460q59 0 113.5 16.5T696-734l-76 48q-33-17-68.5-25.5T480-720q-133 0-226.5 93.5T160-400q0 42 11.5 83t32.5 77h552q23-38 33.5-79t10.5-85q0-36-8.5-70T766-540l48-76q30 47 47.5 100T880-406q1 57-13 109t-41 99q-11 18-30 28t-40 10H204q-21 0-40-10t-30-28q-26-45-40-95.5T80-400q0-83 31.5-155.5t86-127Q252-737 325-768.5T480-800Zm7 313Z" />
+              </svg>
+              <p className="ml-1">{tempo_string + " bpm"}</p>
+            </div>
+
+            {/* Key */}
+            <div className="flex w-32 space-x-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="17px"
+                viewBox="0 -960 960 960"
+                width="17px"
+                fill="#6b7280"
+              >
+                <path d="M400-120q-66 0-113-47t-47-113q0-66 47-113t113-47q23 0 42.5 5.5T480-418v-422h240v160H560v400q0 66-47 113t-113 47Z" />
+              </svg>
+              <p className="ml-1">{keysig_string}</p>
+            </div>
           </div>
-        </div>
+        )}
       </a>
 
       {/* Open Entry */}

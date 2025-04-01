9c347407809e4f901ff87baafeaf2f7249e54b77
TroyKurniawan <TroyKurniawan97@gmail.com>
2025-03-31 22:46:46 -0700

    Added Bandcamp to footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,7 @@ import am_icon from "../icons/social-am.png";
 import tw_icon from "../icons/social-tw.png";
 import ig_icon from "../icons/social-ig.png";
 import tt_icon from "../icons/social-tt.png";
+import bc_icon from "../icons/social-bc.png";
 import SocialMediaButton from "./utilities/SocialMediaButton";
 import { currentYear } from "../App";
 
@@ -16,6 +17,7 @@ let am_link = "https://music.apple.com/ca/artist/similar-outskirts/1073747592";
 let tw_link = "https://x.com/Sim_Out_";
 let ig_link = "https://instagram.com/similaroutskirts";
 let tt_link = "https://www.tiktok.com/@similaroutskirts";
+let bc_link = "https://similaroutskirts.bandcamp.com";
 
 const Footer = () => {
   return (
@@ -24,11 +26,12 @@ const Footer = () => {
                 bg-black
                 grid content-center"
     >
-      <div className="flex justify-center scale-75 md:scale-90">
+      <div className="flex justify-center scale-90">
         <SocialMediaButton link={sc_link} icon={sc_icon} />
         <SocialMediaButton link={yt_link} icon={yt_icon} />
         <SocialMediaButton link={sp_link} icon={sp_icon} />
         <SocialMediaButton link={am_link} icon={am_icon} />
+        <SocialMediaButton link={bc_link} icon={bc_icon} />
         <SocialMediaButton link={tw_link} icon={tw_icon} />
         <SocialMediaButton link={ig_link} icon={ig_icon} />
         <SocialMediaButton link={tt_link} icon={tt_icon} />

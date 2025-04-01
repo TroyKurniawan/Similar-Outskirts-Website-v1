e2c25b7cc093bfe0ec1d4e587ab8bbfdad674251
TroyKurniawan <TroyKurniawan97@gmail.com>
2025-03-31 23:27:55 -0700

    Changed header buttons to stay white on hover.

--- src/components/utilities/ButtonHeader.tsx
+++ src/components/utilities/ButtonHeader.tsx
@@ -8,7 +8,7 @@ function ButtonHeader({ title, link }: ButtonProps) {
     <a
       href={link}
       className="py-4 px-4 mx-2
-                hover:text-blue-500 border-blue-500 hover:border-b-4 transition-all
+               border-blue-500 hover:border-b-4 transition-all
                 flex
                 text-lg font-bold
                 hover:bg-white hover:bg-opacity-5
